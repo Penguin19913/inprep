28f4e945d633fc39e0e4731ba2c556629ac92750
Penguin19913 <abhishekmaurya102515k@gmail.com>
2026-07-15 12:36:55 +0530

    editing server

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ await connectDB()
 await connectCloudinary()
 
 // Middlewares
-app.use(cors({
-}))
+app.use(cors({origin: 'https://inprep-murex.vercel.app'}))
 app.use(clerkMiddleware())
 
 // Routes
